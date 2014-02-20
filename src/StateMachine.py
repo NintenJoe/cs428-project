45241ee2d22b804d968efd04d1a42f97d108d847
Joseph Ciurej <ciurej2@illinois.edu>
2014-02-20 00:47:13 -0600

    Added the module and test files for the state machine type.

---
+++ src/StateMachine.py
@@ -0,0 +1,36 @@
+##	@file StateMachine.py
+#	@author Joseph Ciurej
+#	@date Spring 2014
+#
+#	Source File for the "StateMachine" Type
+#
+#	@TODO
+#	- Write the implementation in this file!
+
+##	An implementation of the finite state machine pattern, which is used to 
+#	facilitate state representation and transition specification for game
+#	world objects.  A state machine resembles a graph in which nodes are
+#	object states and edges are directed and activated by events occuring
+#	within the game world.
+class StateMachine():
+	### Constructors ###
+
+	##	
+	#	
+	def __init__( self,  ):
+		pass
+
+	### Methods ###
+
+	##	
+	#	
+	def method( self,  ):
+		pass
+
+	### Helper Functions ###
+
+	##	
+	#	
+	def _method( self,  ):
+		pass
+
---
+++ test/StateMachineTests.py
@@ -0,0 +1,27 @@
+##	@file StateMachineTests.py
+#	@author Joseph Ciurej
+#	@date Spring 2014
+#
+#	Test File for the "StateMachine" Type
+#
+#	@TODO
+#	- Write the implementation in this file!
+
+import unittest
+
+##	Container class for the test suite that tests the functionality of the
+#	"StateMachine" type.
+class StateMachineTest( unittest.TestCase ):
+	### Test Set Up/Tear Down ###
+
+	def setUp( self ):
+		pass
+
+	def tearDown( self ):
+		pass
+
+	### Testing Functions ###
+
+	def testTest( self ):
+		self.assertEqual( True, True, "" )
+
