efaf75408635bdd4e50ff024692de98d000fd923
Joseph Ciurej <ciurej2@illinois.edu>
2014-03-06 01:00:00 -0600

    Created the 'Transition' type and all associated tests (refinement of 'StateMachine' type).

---
+++ test/TransitionTests.py
@@ -0,0 +1,60 @@
+##  @file TransitionTests.py
+#   @author Joseph Ciurej
+#   @date Spring 2014
+#
+#   Test File for the "Transition" Type
+#
+#   @TODO
+
+import unittest
+import src
+from src.Event import *
+
+##  Container class for the test suite that tests the functionality of the
+#   "Transition" type.
+class TransitionTest( unittest.TestCase ):
+    ### Testing Constants ###
+
+    ##  The string identifier for the source node in the test transition.
+    SRC_NAME = "source"
+
+    ##  The string identifier for the destination node in the test transition.
+    DST_NAME = "destination"
+
+    ##  The only event that will invoke the test transition.
+    TRANS_EVENT = Event( EventType.NOTIFY )
+
+    ### Test Set Up/Tear Down ###
+
+    def setUp( self ):
+        self._transition = Transition( TransitionTest.SRC_NAME,
+            TransitionTest.DST_NAME, TransitionTest.TRANS_EVENT )
+
+    def tearDown( self ):
+        self._transition = None
+
+    ### Testing Functions ###
+
+    def test_constructor( self ):
+        self.assertEqual( self._transition.get_source(), TransitionTest.SRC_NAME,
+            "Incorrect source node identifier on default construction." )
+        self.assertEqual( self._transition.get_destination(), TransitionTest.DST_NAME,
+            "Incorrect destination node identifier on default construction." )
+
+    def test_invocation_by_valid_events( self ):
+        valid_event = TransitionTest.TRANS_EVENT
+
+        self.assertEqual( self._transition.invoked_by( valid_event ), True,
+            "Transition not invoked by a valid invocation." )
+
+    def test_invocation_by_invalid_events( self ):
+        difftype_event = Event( EventType.COLLISION )
+
+        self.assertEqual( self._transition.invoked_by( difftype_event ), False,
+            "Transition invoked by an invalid invocation (different event type)." )
+
+        diffparam_event = Event( EventType.NOTIFY, { "derp" : None } )
+
+        self.assertEqual( self._transition.invoked_by( diffparam_event ), False,
+            "Transition invoked by an invalid invocation (different event params)." )
+
