f6af236d4e63b56e6768ff2cd70248aa1e9aa696
Josh Halstead <jhalstead85@gmail.com>
2014-03-06 18:52:44 -0600

    Added documentation. Task 6.2 complete

--- src/SpatialDictionary.py
+++ src/SpatialDictionary.py
@@ -4,6 +4,17 @@
 #
 #   A spatial hashing collision detection strategy.
 #
+#   Given the dimensions of some part of the world, we overlay a grid of
+#   virtual cells onto it. The dimensions of the square cells are uniform and
+#   user-specified. Each cell may or may not contain game world entities, and
+#   some game world entities may overlap multiple cells. From the cells, we
+#   form a linear hashtable where each cell is identified by an integer index.
+#   Game world entities will hash to said index if they exist (at all) inside
+#   of the cell. For a given entity A, we can find its nearby neighbors by
+#   hashing entity A and retrieving the other entities in that index. Then we
+#   check if any of those objects collide with entity A. We do this for all
+#   entities in the hash table to compute all collisions in the world.
+#
 #   Assumption: Width is a multiple of cell_size.
 
 from CollisionDetector import *
@@ -12,20 +23,40 @@ from HashableRect import *
 
 class SpatialDictionary(CollisionDetector):
 
-    # Public methods
+    ### Construtors ###
 
+    ## Constructs a spatial hashing dictionary with a given cell size and
+    #  width/height for detecting collisions amongst a set of bonding volumes.
+    #
+    #  @param cell_size An integer specifying the square dimensions of a cell
+    #   in the game world. The cell size should be a factor of the width.
+    #  @param width An integer specifying the total width of the area for which
+    #   we're detecting collisions.
+    #  @param height An integer specifying the total height of the area for
+    #   which we're detecting collisions.
     def __init__(self, cell_size=1, width=1, height=1):
+        # Initialize the super class
         CollisionDetector.__init__(self)
 
+        # Configure the virtual grid
         self.cell_size = cell_size
         self.width = width
         self.height = height
+
+        # Number of columns in the virtual grid
         self.columns = width/cell_size
+
+        # Hashtable mapping a cell to a set of game world entities that exist
+        # in said cell
         self.table = {}
 
         # Set of the objects (i.e. bounding volumes) being tracked
         self.objects = set([])
 
+    ### Public methods ###
+
+    ## Returns a string representation of the spatial hashing dictionary. This
+    #  is intended to be machine-readable.
     def __repr__(self):
         rep = "{"
         for cell in self.table:
@@ -34,6 +65,8 @@ class SpatialDictionary(CollisionDetector):
 
         return rep + "}"
 
+    ## Returns a string representation of the spatial hashing dictionary. THis
+    #  is intended to be human-readable
     def __str__(self):
         rep = "{"
         for cell in self.table:
@@ -43,12 +76,24 @@ class SpatialDictionary(CollisionDetector):
         # Trim the last newline before appending the closing brace.
         return rep[:-1] + "}"
 
+    ## Adds a list of bounding volumes to the dictionary. These bounding 
+    #  represent the hitboxes of the game world entities.
+    #
+    #  Note: The bounding volumes MUST be hashable. 
+    #
+    #  @param objs A list of _hashable_ bounding volumes
     def add_multiple(self, objs):
         self.objects.update(objs)
 
         for obj in objs:
             self.add(obj)
 
+    ## Adds a single bounding volume to the dictionary. The bounding volume
+    #  represents the hitbox of a game world entity.
+    #
+    #  Note: The bounding volume MUST be hashable.
+    #
+    #  @param obj A single bounding volume.
     def add(self, obj):
         self.objects.add(obj)
 
@@ -56,12 +101,20 @@ class SpatialDictionary(CollisionDetector):
         for cell in cells:
             self._add(cell, obj)
 
+    ## Removes a list of bounding volumes from the dictionary. The bounding
+    #  volumes represent the hitboxes of the game world entities.
+    #
+    #  @param objs The list of bounding volumes to be removed.
     def remove_multiple(self, objs):
         self.objects -= set(objs)
 
         for obj in objs:
             self.remove(obj)
 
+    ## Removes a single bounding volumes from the dictionary. The bounding
+    #  volume represents the hitbox of the game world entity.
+    #
+    #  @param objs The single bounding volumes to be removed.
     def remove(self, obj):
         self.objects.discard(obj)
 
@@ -69,6 +122,11 @@ class SpatialDictionary(CollisionDetector):
         for cell in cells:
             self._remove(cell, obj)
 
+    ## Determines all the collisions that are occuring given the current state
+    #  of the spatial hashing dictionary.
+    #
+    #  @return A list of (Bounding Volume, Bounding Volume) pairs of colliding
+    #   game world entities.
     def get_all_collisions(self):
         collisions = set([])
         for obj in self.objects:
@@ -79,9 +137,12 @@ class SpatialDictionary(CollisionDetector):
 
         return list(collisions)
 
+    ## @return A list of all the bounding volumes currently being tracked.
     def get_all_objects(self):
         return list(self.objects)
 
+    ## @return True if we're tracking the provided bounding volume and false
+    #   otherwise.
     def exists(self, obj):
         cells = self._get_covered_cells(obj)
         for cell in cells:
@@ -90,29 +151,46 @@ class SpatialDictionary(CollisionDetector):
 
         return True
 
+    ## @return The number of objects in the dictionary.
     def size(self):
         return len(self.objects)
 
+    ## Resets the spatial hashing dictionary.
     def clear(self):
         self.objects = set([])
         self.table.clear()
 
-    # Private helper methods
+    ### Private helper methods ###
 
+    ## Adds a bounding volume to a cell.
+    #
+    #  @param cell The cell index that contains the bounding volume.
+    #  @param obj The bounding volume that we're adding to the cell.
     def _add(self, cell, obj):
         if cell in self.table:
             self.table[cell].add(obj)
         else:
             self.table.setdefault(cell, set()).add(obj)
 
+    ## Removes a bounding volume from a cell.
+    #
+    #  @param cell The cell index that contains the bounding volume.
+    #  @param obj The bounding volume that we're removing from the cell.
     def _remove(self, cell, obj):
         if cell in self.table:
             self.table[cell].discard(obj)
 
+    ## Maps a given (x, y) position in the game world, to a cell's index in the
+    #  spatial dictionary.
+    #
+    #  @param x The x-coordinate
+    #  @param y The y-coordinate
     def _hash(self, x, y):
         return (int(x/self.cell_size) +
                 int(y/self.cell_size) * self.columns)
 
+    ## @return A list of bounding volumes that are in the same cell(s) as
+    #   the given one.
     def _get_nearby_objects(self, obj):
         nearby_objects = set([])
         cells = self._get_covered_cells(obj)
@@ -123,7 +201,7 @@ class SpatialDictionary(CollisionDetector):
         nearby_objects.discard(obj)
         return list(nearby_objects)
 
-    # Returns a list of cells that a bounding volume overlaps
+    ## @return A list of cells that a given bounding volume overlaps.
     def _get_covered_cells(self, obj):
         x = obj.x
         y = obj.y
@@ -143,9 +221,17 @@ class SpatialDictionary(CollisionDetector):
 
         return list(covered_cells)
 
+    ## Computes the number of rows in the grid that a bounding volume overlaps.
+    #
+    #  @param start The cell that contains the top left corner of the bounding
+    #   volume.
+    #  @param stop The cell that contains the bottom left corner of the
+    #   bounding volume.
+    #  @return The number of rows a bounding volume overlaps.
     def _get_num_covered_rows(self, start, stop):
         return (stop - start)/self.columns + 1
 
+    ## @return A set containing all the bounding volumes in a given cell.
     def _objs_in(self, cell):
         if cell in self.table:
             return self.table[cell]
