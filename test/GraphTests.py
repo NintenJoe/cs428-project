d64d179cd55f8395d43fdf69a472d78de65b1699
Nick <nickljeffrey@gmail.com>
2014-03-27 16:03:45 -0500

    Started writing tests

---
+++ test/GraphTests.py
@@ -0,0 +1,26 @@
+##  @file GraphTests.py
+#   @author Nick Jeffrey
+#   @date Spring 2014
+#
+#   Test File for the "Graph" Type
+#
+#   @TODO
+#   - Cover at least 90% of code in Graph
+
+import unittest
+import src
+
+class GraphTest( unittest.TestCase ):
+
+    ### Test Set Up/Tear Down ###
+
+    def setUp( self ):
+        pass
+
+    def tearDown( self ):
+        pass
+
+    ### Testing Functions ###
+
+    def test_default_constructor( self ):
+        pass
---
+++ test/PlayerTests.py
@@ -0,0 +1,25 @@
+##  @file PlayerTests.py
+#   @author Nick Jeffrey
+#   @date Spring 2014
+#
+#   Test File for the "Player" Type
+#
+#   @TODO
+#   - Cover at least 90% of code in Player and Entity
+import unittest
+import src
+
+class PlayerTest( unittest.TestCase ):
+
+    ### Test Set Up/Tear Down ###
+
+    def setUp( self ):
+        pass
+
+    def tearDown( self ):
+        pass
+
+    ### Testing Functions ###
+
+    def test_player_constructor( self ):
+        pass
