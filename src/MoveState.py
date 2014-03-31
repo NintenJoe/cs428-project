ac4f954821dacd1e8af4cdc143ec5f3733276571
Nick <nickljeffrey@gmail.com>
2014-03-30 20:59:51 -0500

    updated MoveState

--- src/MoveState.py
+++ src/MoveState.py
@@ -42,7 +42,7 @@ class MoveState( State ):
     def _calc_step_changes( self, time_delta ):
         deltax = self._vx * time_delta;
         deltay = self._vy * time_delta;
-        phys_delta = PhysicalState( PG.Rect(0, 0, deltax, deltay), (0, 0), 0.0 )
+        phys_delta = PhysicalState( PG.Rect(deltax, deltay, 0, 0), (0, 0), 0.0 )
 
         return SimulationDelta( phys_delta )
 
@@ -50,11 +50,11 @@ class MoveState( State ):
     #
     #   @override
     def _calc_arrival_changes( self ):
-        return SimulationDelta()
+        return SimulationDelta(PhysicalState(PG.Rect(0, 0, 0, 0), (self._vx, self._vy), 0.0))
 
     ##  Returns an empty set of physical changes.
     #
     #   @override
     def _calc_departure_changes( self ):
-        return SimulationDelta()
+        return SimulationDelta(PhysicalState(PG.Rect(0, 0, 0, 0), (-self._vx, -self._vy), 0.0))
 
