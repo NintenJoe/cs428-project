859cb6292b59992d0e9ce0c16c19e455d558b80a
Edwin Chan <chan100@illinois.edu>
2014-03-31 02:13:21 -0500

    updated gameview

--- src/GameView.py
+++ src/GameView.py
@@ -25,7 +25,7 @@ from Globals import *
 class GameView():
     ### Constructors ###
 
-    ##  
+    ##  Constructs a Game View that will set up the game screen
     def __init__(self):
         self._screen = PG.display.set_mode( (640, 480) )
 
@@ -37,7 +37,11 @@ class GameView():
 
     ### Methods ###
 
-    ##  
+    ##  Draws the Game World within the camera's viewport.
+    #
+    #   @param game_world   Game World, which contains information about the world
+    #                        and all the entities contained in it.
+    
     def render( self, game_world ):
         self._screen.fill( (0, 0, 0) )
 
@@ -50,7 +54,16 @@ class GameView():
 
         PG.display.flip()
 
-    ##  
+    ##  Render the environment by reading in a tilemap and a camera.
+    #
+    #   The function works by reading in a set of tilemaps and then sets up a
+    #   dictionary that will map the tile key to a tile image. Then it will
+    #   only draw the tiles that are within the camera's viewport.
+    #
+    #   @param viewport     The camera's view, should be a pygame rect
+    #   @param tilemap      2D Array of strings that contain the all the
+    #                        information about each tile.
+    
     def render_environment( self, viewport, tilemap ):
         self._load_tiles_for_tilemap( tilemap )
 
@@ -71,7 +84,11 @@ class GameView():
 
                 self._screen.blit( self._loaded_tiles[tile_id], (tile_pos_x, tile_pos_y) )
 
-    ##  
+    ##  Renders all the entities (player and monsters) onto the screen.
+    #
+    #   @param viewport     The camera's view, should be a pygame rect
+    #   @param entity_list  List of all entities and their information
+    
     def render_entities( self, viewport, entity_list ):
         self._load_entities( entity_list )
 
@@ -85,7 +102,11 @@ class GameView():
 
             self._screen.blit( entity_animation.get_frame(game_time), (entity_pos_x, entity_pos_y) )
 
-    ##  
+    ##  Creates a dictionary called loaded_tiles which maps a tile key to an image
+    #
+    #   @param tilemap      2D Array of strings that contain the all the
+    #                        information about each tile.
+    
     def _load_tiles_for_tilemap( self, tilemap ):
         for idx_x in range( len(tilemap) ):
             for idx_y in range( len(tilemap[0]) ):
@@ -94,7 +115,11 @@ class GameView():
                 if tile_id not in self._loaded_tiles:
                       self._loaded_tiles[ tile_id ] = load_image( ASSET_PATH + "/graphics/tiles/" + tile_id + ".bmp")
 
-    ##  
+    ##  Creates a dictionary called loaded_entities which maps an entity key to a
+    #   its corresponding sprite sheet.
+    #
+    #   @param entity_list  List of all entities and their information
+    
     def _load_entities( self, entity_list ):
         for entity in entity_list:
             entity_key = self._get_entity_key( entity )
@@ -112,12 +137,22 @@ class GameView():
             if entity_key not in self._loaded_entities:
                 self._loaded_entities[ entity_key ] = Animation( entity_path, frame_count, 33, True )
 
-    ##  
+    ##  Helper function that helps gather the entities' dictionary key
+    #
+    #   @param entity       The entity in which we need information from
+    #
+    #   @return             The key which is a combination of an entity's name and state
+    
     def _get_entity_key( self, entity ):
         entity_info = entity.get_status().split()
         return entity_info[0] + " " + entity_info[1]
 
-    ##  
+    ##  Helper function that helps gather the entities' filepath
+    #
+    #   @param entity       The entity in which we need information from
+    #
+    #   @return             The filepath for the entity's spritesheet
+    
     def _get_entity_path( self, entity ):
         entity_info = entity.get_status().split()
         return ASSET_PATH + "/graphics/entities/" + entity_info[0] + "/" + entity_info[1] + ".png"
