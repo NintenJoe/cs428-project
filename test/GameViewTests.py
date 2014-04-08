7988716fffd26d3e297cb778fd57e813a02cde6b
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-08 01:23:46 -0500

    Refined the mock unit testing to support patch mocks.

--- test/GameViewTests.py
+++ test/GameViewTests.py
@@ -13,9 +13,9 @@
 
 import unittest
 import mock
-import pygame as PG
 
 import src
+from src.Globals import *
 from src.GameView import *
 from src.GameWorld import *
 
@@ -24,24 +24,29 @@ from src.GameWorld import *
 class GameViewTests( unittest.TestCase ):
     ### Test Set Up/Tear Down ###
 
-    @classmethod
-    def setUpClass( cls ):
-        PG.display.init = mock.MagicMock()
-        PG.display.quit = mock.MagicMock()
-
-        PG.display.set_mode = mock.MagicMock()
-        PG.display.set_caption = mock.MagicMock()
-        PG.display.flip = mock.MagicMock()
-
     def setUp( self ):
-        pass
+        self._patchers = [
+            mock.patch("pygame.display.set_mode"),
+            mock.patch("pygame.display.set_caption"),
+            mock.patch("pygame.display.flip"),
+        ]
+        self._setmode_mock = self._patchers[ 0 ].start()
+        self._setcapt_mock = self._patchers[ 1 ].start()
+        self._flip_mock = self._patchers[ 2 ].start()
+
+        self._view = GameView()
 
     def tearDown( self ):
-        pass
+        self._view = None
+        self._setmode_mock = None
+        self._setcapt_mock = None
+        self._flip_mock = None
+
+        [ patcher.stop() for patcher in self._patchers ]
 
     ### Testing Functions ###
 
-    def test_test( self ):
-        view = GameView()
-        self.assertEqual( True, True, "" )
+    def test_initialization( self ):
+        self._setmode_mock.assert_called_once_with( (640, 480) )
+        self._setcapt_mock.assert_called_once_with( GAME_NAME )
 
