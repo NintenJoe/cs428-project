8deb5f96676e17fd90222b2f7cb86608c78f7a8c
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-09 01:37:17 -0500

    Completed all the basic tests for the 'GameView' type using mock unit testing.

--- test/GameViewTests.py
+++ test/GameViewTests.py
@@ -4,17 +4,20 @@
 #
 #   Test File for the "GameView" Type
 #
-#   @see http://www.voidspace.org.uk/python/mock/examples.html#nesting-patches
-#
 #   @TODO
+#   High Priority:
 #   - Refine the tests to assert that only the `Entity` objects that are within
 #     the scope of the game viewport are rendered once this functionality is
 #     implemented.
+#   Low Priority:
 #   - Refactor the functionality associated with the `DISPLAY_PATCHERS` to
 #     make the code less clunky.
+#   - Update the `GameViewTests.WORLD_TILES` matrix to contain `Tile` instances
+#     instead of strings.
 
 import unittest
 import mock
+import src.Globals
 
 from src.GameView import GameView
 from src.GameWorld import GameWorld
@@ -37,8 +40,9 @@ class GameViewTests( unittest.TestCase ):
 
     ##  A matrix containing the `Tile` objects given by the mock `GameWorld`.
     WORLD_TILES = [
-        [ "0", "1", "2" ],
-        [ "3", "4", "5" ],
+        [ "0", "1" ],
+        [ "2", "3" ],
+        [ "4", "5" ],
     ]
 
     ##  A list containing the test `Entity` objects given by the mock `GameWorld`.
@@ -60,6 +64,8 @@ class GameViewTests( unittest.TestCase ):
         self._flip_mock = GameViewTests.DISPLAY_PATCHERS[ "flip" ].start()
         self._load_image_mock = GameViewTests.DISPLAY_PATCHERS[ "load_image" ].start()
 
+        self._display_mock = self._setmode_mock.return_value
+
         self._view = GameView()
 
     def tearDown( self ):
@@ -76,16 +82,81 @@ class GameViewTests( unittest.TestCase ):
         self.assertTrue( self._setcapt_mock.called,
             "Game view doesn't properly initialize PyGame display caption." )
 
+
     def test_render_initialization( self ):
         self._view.render( self._world_mock )
 
-        self.assertTrue( self._setmode_mock.return_value.fill.called,
+        self.assertTrue( self._display_mock.fill.called,
             "Game view doesn't properly clear view before rendering." )
         self.assertTrue( self._flip_mock.called,
             "Game view doesn't properly flip the graphics buffer after rendering." )
 
+
     def test_render_loading( self ):
+        for entity in GameViewTests.WORLD_ENTITIES:
+            self.assertFalse( self._view._is_entity_graphic_loaded(entity),
+                "Game view improperly loads entity assets before they're needed." )
+        for tile in [ t for s in GameViewTests.WORLD_TILES for t in s ]:
+            self.assertFalse( self._view._is_tile_graphic_loaded(tile),
+                "Game view improperly loads tile assets before they're needed." )
+
         self._view.render( self._world_mock )
 
-        self.assertEqual( True, True, "" )
+        for entity in GameViewTests.WORLD_ENTITIES:
+            self.assertTrue( self._view._is_entity_graphic_loaded(entity),
+                "Game view doesn't load entity assets when they're rendered." )
+        for tile in [ t for s in GameViewTests.WORLD_TILES for t in s ]:
+            self.assertTrue( self._view._is_tile_graphic_loaded(tile),
+                "Game view doesn't load tile assets when they're rendered." )
+
+
+    def test_render_onetile_environment( self ):
+        self._world_mock.get_viewport.return_value = HashableRect( 0, 0,
+             src.Globals.TILE_DIMS[0] - 1, src.Globals.TILE_DIMS[1] - 1 )
+        self._view.render( self._world_mock )
+
+        blit_calls = self._display_mock.blit.call_args_list
+        self.assertEqual( len(blit_calls), 1,
+            "Game view renders too many environment tiles in simple cases." )
+
+        blit_call = blit_calls[0]
+        self.assertEqual( blit_call[0][0], self._view._get_tile_graphic("0"),
+            "Game view renders incorrect tiles in simple cases." )
+        self.assertEqual( blit_call[0][1], (0, 0),
+            "Game view renders improperly offsets tile graphics in simple cases." )
+
+
+    def test_render_multitile_environment( self ):
+        viewport_offset = (
+            int(src.Globals.TILE_DIMS[0] / 2),
+            int(src.Globals.TILE_DIMS[1] / 2)
+        )
+        viewport_dims = (
+            src.Globals.TILE_DIMS[0],
+            src.Globals.TILE_DIMS[1]
+        )
+        self._world_mock.get_viewport.return_value = HashableRect(
+             viewport_offset[0], viewport_offset[1],
+             viewport_dims[0], viewport_dims[1]
+        )
+        self._view.render( self._world_mock )
+
+        blit_calls = self._display_mock.blit.call_args_list
+        self.assertEqual( len(blit_calls), 4,
+            "Game view renders too few environment tiles in complex cases." )
+
+        self.assertEqual(
+            set( [blit_call[0][0] for blit_call in blit_calls] ),
+            set( [self._view._get_tile_graphic(str(i)) for i in range(4)] ),
+            "Game view renders incorrect tiles in complex cases."
+        )
+        self.assertEqual(
+            blit_calls[0][0][1],
+            (0 - viewport_offset[0], 0 - viewport_offset[1]),
+            "Game view renders improperly offsets tile graphics in complex cases."
+        )
+
+
+    def test_render_entities( self ):
+        pass
 
