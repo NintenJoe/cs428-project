41eeed4ad24c0f2cb48986ad18633368fcb4daac
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-24 03:32:51 -0500

    Refactored the 'CompositeHitbox' type to include the 'adopt_template' operation, which should help eliminate code in the 'Entity' class.

--- src/CompositeHitbox.py
+++ src/CompositeHitbox.py
@@ -27,6 +27,7 @@
 #     composite hitboxes will be read in from file).
 #   - Make the implementation of the equality operator in the `CompositeHitbox`
 #     type more elegant.
+#   - Add testing for the `Hitbox` type's `__repr__` function.
 
 import itertools
 import pygame as PG
@@ -48,6 +49,9 @@ class HitboxType():
     #   TODO: Update or remove this hitbox classification.
     HURT = "hurt"
 
+    ##  Indicates that the described hitbox surrounds an intangible object.
+    INTANGIBLE = "intangible"
+
 
 ##  The representation of a rectangular, axis-aligned collision volume within 
 #   the game world.
@@ -68,15 +72,25 @@ class Hitbox( PG.Rect ):
 
     ### Overloaded Operators ###
 
+    ##  @return The hitbox type of the instance hitbox.
+    def __repr__( self ):
+        return str(self.htype)
+
     ##  @return The unique hashing integer identifier for the hitbox instance.
     def __hash__( self ):
         return id( self )
 
-    ## @TODO: Add testing for this function.
-    def __repr__( self ):
-        return str(self.htype)
-
     ### Methods ###
+
+    ##  Copies the contents of the given `Hitbox` into the instance `Hitbox`.
+    #
+    #   @param hitbox The `Hitbox` instance to have its values copied.
+    def copy_ip( self, hitbox ):
+        self.x = hitbox.x
+        self.y = hitbox.y
+        self.w = hitbox.w
+        self.h = hitbox.h
+        self._type = hitbox._type
 
     ##  @return The classification attributed to the hitbox (of type `HitboxType`).
     @property
@@ -103,17 +117,10 @@ class CompositeHitbox( object ):
     #   @param hitbox_list A list of `Hitbox` objects that will compose the
     #    composite (positioned relative to the composite's origin).
     def __init__( self, pos_x=0, pos_y=0, hitbox_list=[] ):
-        self._container_box = Hitbox(
-            pos_x,
-            pos_y,
-            max( [hb.x + hb.w for hb in hitbox_list] ) if hitbox_list else 0,
-            max( [hb.y + hb.h for hb in hitbox_list] ) if hitbox_list else 0,
-        )
+        self._container_box = Hitbox( pos_x, pos_y, 0, 0 )
+        self._inner_boxes = [ Hitbox(0, 0, 0, 0) for i in range(6) ]
 
-        self._inner_boxes = [
-            Hitbox( hb.x + pos_x, hb.y + pos_y, hb.w, hb.h, hb.htype ) \
-            for hb in hitbox_list
-        ]
+        self._adjust_boxes_to( hitbox_list )
 
     ### Overloaded Operators ###
 
@@ -125,6 +132,13 @@ class CompositeHitbox( object ):
             all(self._inner_boxes.count(i) == other._inner_boxes.count(i) for i in self._inner_boxes)
 
     ### Methods ###
+
+    ##  Transforms the instance composite to adopt the composite template given,
+    #   adjusting the instance box structure (but not position) to match template.
+    #
+    #   @param chitbox_template The template to be adopted by the instance.
+    def adopt_template( self, chitbox_template ):
+        self._adjust_boxes_to( chitbox_template.get_inner_boxes_relative() )
 
     ##  Translates the composite hitbox by the given amount along the two
     #   cardinal axes.
@@ -139,26 +153,54 @@ class CompositeHitbox( object ):
             inner_box.x += delta_x
             inner_box.y += delta_y
 
-    ##  @return A listing of all `Hitbox` objects that represents the inner hitboxes
-    #    of the composite relative to the composite origin.
-    def get_relative_hitboxes( self ):
-        comp_pos = self.get_position()
-
-        return [
-            Hitbox(hb.x - comp_pos[0], hb.y - comp_pos[1], hb.w, hb.h) \
-            for hb in self._inner_boxes
-        ]
-
     ##  @return The position of the composite as a tuple of the form (x, y).
     def get_position( self ):
         return ( self._container_box.x, self._container_box.y )
 
     ##  @return A `Hitbox` container for all the hitboxes that compose the composite.
-    def get_hitbox( self ):
+    def get_bounding_box( self ):
         return self._container_box
 
     ##  @return A listing of all the `Hitbox` objects that compose the composite.
-    def get_hitboxes( self ):
+    def get_inner_boxes( self ):
         return self._inner_boxes
 
+    ##  @return A listing of all `Hitbox` objects that represents the inner hitboxes
+    #    of the composite relative to the composite origin.
+    def get_inner_boxes_relative( self ):
+        chitbox_pos = self.get_position()
+
+        return [
+            Hitbox(hb.x - chitbox_pos[0], hb.y - chitbox_pos[1], hb.w, hb.h) \
+            for hb in self._inner_boxes
+        ]
+
+    ### Helper Methods ###
+
+    ##  Adjusts the hitboxes contained in the composite to match the given list
+    #   of hitboxes.
+    #
+    #   @param hitbox_list A listing of `Hitbox` instances to be mirrored by
+    #    the instance's hitbox list.
+    def _adjust_boxes_to( self, hitbox_list ):
+        chitbox_pos = self.get_position()
+
+        # Reset Inner Hitboxes #
+        for inner_hitbox in self._inner_boxes:
+            inner_hitbox.copy_ip( Hitbox(0, 0, 0, 0, HitboxType.INTANGIBLE) )
+            inner_hitbox.move_ip( chitbox_pos[0], chitbox_pos[1] )
+
+        # Update Inner Hitboxes to Mirror Given List #
+        for hitbox_idx in range( 0, len(hitbox_list) ):
+            inner_hitbox = self._inner_boxes[ hitbox_idx ]
+            new_hitbox = hitbox_list[ hitbox_idx ]
+
+            inner_hitbox.copy_ip( new_hitbox )
+            inner_hitbox.move_ip( chitbox_pos[0], chitbox_pos[1] )
+
+        # Update Containing Hitbox #
+        self._container_box.copy_ip( Hitbox(chitbox_pos[0], chitbox_pos[1], 0, 0) )
+        self._container_box.unionall_ip(
+            [ b for b in self._inner_boxes if b.htype != HitboxType.INTANGIBLE ]
+        )
 
